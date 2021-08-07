30cdd04ea7b7a8ecbc78ee519fb7c4a343714d56
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-08-07 00:37:31 -0700

    correct the getMonth index

--- Chapter5/complex-app/frontend-js/modules/search.js
+++ Chapter5/complex-app/frontend-js/modules/search.js
@@ -91,7 +91,7 @@ export default class Search {
        console.log(postDate)
       return `<a href="/post/${post._id}" class="list-group-item list-group-item-action">
       <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
-      <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth()}/${postDate.getDate()}/${postDate.getFullYear()}</span>
+      <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth() + 1}/${postDate.getDate()}/${postDate.getFullYear()}</span>
     </a>`
 
      }).join('')} <!--join is the separating char.-->
